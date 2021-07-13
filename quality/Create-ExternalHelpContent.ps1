c5c0c275b106ddcbcddb8be34020b1fbf772e22a
lmissel <39304706+lmissel@users.noreply.github.com>
2021-07-13 22:26:33 +0200

    Update Create-ExternalHelpContent.ps1

--- quality/Create-ExternalHelpContent.ps1
+++ quality/Create-ExternalHelpContent.ps1
@@ -1,8 +1,8 @@
 ﻿$version = "1.1.0.0"
 $language = "en-US"
 
-$MarkdownFolder = ("C:\Users\LarsM\Documents\GitHub\System.IO.Ports.Commands\docs\{0}" -f $language)
-$OutputPath = ("C:\Users\LarsM\Documents\GitHub\System.IO.Ports.Commands\module\{0}\{1}" -f $version, $language)
+$MarkdownFolder = ("\System.IO.Ports.Commands\docs\{0}" -f $language)
+$OutputPath = ("\System.IO.Ports.Commands\module\{0}\{1}" -f $version, $language)
 
 $file = New-ExternalHelp -Path $MarkdownFolder -OutputPath $OutputPath
 

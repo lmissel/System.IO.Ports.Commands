79f792d1f92f921ab052d7700209746247236919
lmissel <39304706+lmissel@users.noreply.github.com>
2021-07-13 22:28:38 +0200

    Delete quality directory

--- quality/Create-ExternalHelpContent.ps1
+++
@@ -1,9 +0,0 @@
-﻿$version = "1.1.0.0"
-$language = "en-US"
-
-$MarkdownFolder = ("\System.IO.Ports.Commands\docs\{0}" -f $language)
-$OutputPath = ("\System.IO.Ports.Commands\module\{0}\{1}" -f $version, $language)
-
-$file = New-ExternalHelp -Path $MarkdownFolder -OutputPath $OutputPath
-
-#Get-HelpPreview -Path $OutputPath

2b5fa120324a987c206e6778133f7dbb2b57ba55
lmissel <39304706+lmissel@users.noreply.github.com>
2021-07-20 19:52:00 +0200

    1.1.1.0
    Adaptation to the new internal standard of a script module.

---
+++ System.IO.Ports.Commands/1.1.1.0/de-DE/System.IO.Ports.Commands.psd1
@@ -0,0 +1,12 @@
+﻿#requires -Version 2.0
+ConvertFrom-StringData -StringData @'
+ArgumentOutOfRangeException	=	Der offset-Parameter oder der count-Parameter liegt außerhalb eines gültigen Bereichs des übergebenen buffer. Entweder offset oder count ist kleiner als 0 (null).
+ArgumentNullException			=	Der text-Parameter ist null.
+ArgumentException				=	Die Summe von offset und count ist größer als die Länge von buffer.
+Win32Exception					=	Die seriellen Anschlussnamen konnten nicht abgefragt werden.
+IOException					=	Der angegebene Anschluss wurde nicht gefunden oder konnte nicht geöffnet werden.
+TimeoutException				=	Die WriteLine(String)-Methode konnte nicht in den Stream schreiben.
+TimeoutExceptionRead			=	Es waren keine Bytes zum Lesen verfügbar.
+InvalidOperationException		=	Der angegebene Anschluss ist nicht offen.
+INVALID_SOURCE_IDENTIFIER		=	Das Ereignisabonnement für den seriellen Anschluss [{0}] ist nicht vorhanden. Bitte verwende die Open-SerialPort Funktion, um ein seriellen Anschluss zu öffnen.
+'@
--- tests/System.IO.Ports.Commands.Tests.ps1
+++ tests/System.IO.Ports.Commands.Tests.ps1
@@ -1,42 +1,63 @@
-﻿param(
-$oldVersion = "1.0.0.0",
-$version = "1.1.0.0",
-$moduleName = "System.IO.Ports.Commands",
-$StartWith = "C:\Users\LarsM\Documents\GitHub\"
+﻿#requires -Version 2.0 -Modules Microsoft.PowerShell.Utility, Pester, PSScriptAnalyzer, System.IO.Ports.Commands
+
+[CmdletBinding()]
+param(
+  [string]$oldVersion = '1.0.0.0',
+  [string]$version = '1.1.0.0',
+  [string]$moduleName = 'System.IO.Ports.Commands',
+  [string]$StartWith = "$env:USERPROFILE/Documents/GitHub/"
 )
 
-$Repository = "$StartWith\$moduleName"
-$moduleFolder = "$Repository\module"
-$moduleManifestPath = "$moduleFolder\$version\$moduleName.psd1"
-$manifest = Import-PowerShellDataFile -Path $moduleManifestPath -ErrorAction Ignore
+$FolderPath = '{0}\{1}'
+Add-Type -AssemblyName Microsoft.PowerShell.Commands.Utility
 
-Describe "Testing module" {
+$Repository = ($FolderPath -f $StartWith, $moduleName)
+$moduleFolder = ($FolderPath -f $Repository, $moduleName)
+$moduleManifestPath = ('{0}\{1}\{2}.psd1' -f $moduleFolder, $version, $moduleName)
+$manifest = Import-PowerShellDataFile -Path $moduleManifestPath -ErrorAction Stop
+
+Describe -Name 'Testing module' -Fixture {
     
-    it "must have a module manifest with the same as the module" {
-        $moduleManifestPath | should -Exist
-    }
+  It -Name 'must have a module manifest with the same as the module' -Test {
+    $moduleManifestPath | Should -Exist
+  }
 
-    it "must have the description manifest key populated" {
-        $manifest.Description | should -Not -BeNullOrEmpty
-    }
+  It -Name 'must have the description manifest key populated' -Test {
+    $manifest.Description | Should -Not -BeNullOrEmpty
+  }
 
-    it "must have the Autor manifest key populated" {
-        $manifest.Author | should -Not -BeNullOrEmpty
-    }
+  It -Name 'must have the Autor manifest key populated' -Test {
+    $manifest.Author | Should -Not -BeNullOrEmpty
+  }
 
-    it "must have a ModuleVersion manifest key populated" {
-        $manifest.ModuleVersion | should -BeGreaterThan ([version]::new($oldVersion))
-    }
+  It -Name 'must have a ModuleVersion manifest key populated' -Test {
+    $manifest.ModuleVersion | Should -BeGreaterThan -ActualValue ([version]::new($oldVersion))
+  }
 
-    it "must pass Test-ModuleManifest validation" {
-        Test-ModuleManifest -Path $moduleManifestPath | Should -Be $true
-    }
+  It -Name 'must pass Test-ModuleManifest validation' -Test {
+    Test-ModuleManifest -Path $moduleManifestPath | Should -Be -ActualValue $true
+  }
 
-    it "must have associated Pester tests" {
-        "$Repository\tests\$moduleName.Tests.ps1" | Should -Exist
-    }
+  It -Name 'must have associated Pester tests' -Test {
+    ('{0}\tests\{1}.Tests.ps1' -f $Repository, $moduleName) | Should -Exist
+  }
 
-    it "muss pass PSScriptAnalyzer rules" {
-        Invoke-ScriptAnalyzer -Path "$moduleFolder\$version\$moduleName.psm1" -ExcludeRule PSUseDeclaredVarsMoreThanAssignments | should -BeNullOrEmpty
+  It -Name 'muss pass PSScriptAnalyzer rules' -Test {
+    Invoke-ScriptAnalyzer -Path ('{0}\{1}\{2}.psm1' -f $moduleFolder, $version, $moduleName) -ExcludeRule PSUseDeclaredVarsMoreThanAssignments | Should -BeNullOrEmpty
+  }
+}
+
+# describes the function Get-SerialPort
+Describe -Name 'Get-SerialPort' -Fixture {
+
+  # scenario 1: call the function without arguments
+  Context -Name 'Running without arguments'   -Fixture {
+    # test 1: it does not throw an exception:
+    It -Name 'runs without errors' -Test {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-SerialPort } | Should -Not -Throw
     }
+  }
 }
